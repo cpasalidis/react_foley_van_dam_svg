045f6f8322601b9d726b6691e50838a230d8bcbe
cpasalidis <christos.pasalidis@gmail.com>
2017-11-18 17:24:25 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Grid, Navbar, Nav,NavItem,NavDropdown,MenuItem,Jumbotron, Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+           <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">Link</NavItem>
+                <NavItem eventKey={2} href="#">Link</NavItem>
+                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                  <MenuItem eventKey={3.1}>Action</MenuItem>
+                  <MenuItem eventKey={3.2}>Another action</MenuItem>
+                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                </NavDropdown>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">Link Right</NavItem>
+                <NavItem eventKey={2} href="#">Link Right</NavItem>
+              </Nav>
+    </Navbar.Collapse>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
